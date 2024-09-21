98337f4ffc8c261f168200be47235f7e035691f2
Phillip Correa Mittmann <phillip.mittmann@gmail.com>
2024-09-21 09:58:30 -0300

    ajustes tree

--- treetictactoe.py
+++ treetictactoe.py
@@ -3,15 +3,16 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
-tree = pd.read_csv('./tic-tac-toe-processed.data', sep=',', header=0)
+data = pd.read_csv('./tic-tac-toe-processed.data', sep=',', header=0)
 
-X = tree
-y = tree['result']
+X = data
+y = data['result']
 
+# aumentar tamanho do teste
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
 
-classifier = DecisionTreeClassifier()
+tree = DecisionTreeClassifier()
 
-classifier.fit(X_train, y_train)
+tree.fit(X_train, y_train)
 
-y_pred = classifier.predict(X_test)
+y_pred = tree.predict(X_test)
