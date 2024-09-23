0a5413d0bddfc23bc281c090094128acf79fb6bf
Phillip Correa Mittmann <phillip.mittmann@gmail.com>
2024-09-23 02:28:05 -0300

    Novo tree

--- treetictactoe.py
+++ treetictactoe.py
@@ -1,26 +1,33 @@
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
 import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 def inicia_algoritmo():
-    data = pd.read_csv('./tic-tac-toe-processed.data', sep=',', header=0)
+    X = pd.read_csv('./tic-tac-toe-processed.data', sep=',', header=0)
+    y = pd.read_csv('./tic-tac-toe-processed.data', sep=',', header=0)
+    
+    X = X.drop(X.columns[9], axis=1)
+    y = y.drop(y.columns[:9], axis=1)
 
-    X = data
-    y = data['result']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.70)
 
-    # aumentar tamanho do teste
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
 
     tree = DecisionTreeClassifier()
 
-    tree.fit(X_train, y_train)
+    tree.fit(X_train, np.ravel(y_train))
+
+    # y_pred = tree.predict(X_test)
+
+    # accuracy = accuracy_score(y_test, y_pred)
+    # print("Accuracy:", accuracy)
     
-    return tree
 
-def retorna_previsao_tree(tabuleiro):
-    tree = inicia_algoritmo()
-
-    previsao = tree.predict(tabuleiro)
-
-    return previsao
+inicia_algoritmo()
