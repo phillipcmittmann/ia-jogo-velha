5764a6cee0989e3f3eb28a83d167377d456c29f9
Phillip Correa Mittmann <phillip.mittmann@gmail.com>
2024-09-23 01:50:26 -0300

    Ajustes knn

--- cleanData.py
+++ cleanData.py
@@ -27,3 +27,49 @@ output_csv = './tic-tac-toe-no-classes.data'  # Change this to your desired outp
 replace_chars_in_csv(input_csv, output_csv)
 
 ##################################################################################################
+
+# Função para verificar o resultado do jogo
+def verificar_resultado(linha):
+    # Linhas e colunas do tabuleiro de jogo
+    tabuleiro = [
+        [linha['top-left-square'], linha['top-middle-square'], linha['top-right-square']],
+        [linha['middle-left-square'], linha['middle-middle-square'], linha['middle-right-square']],
+        [linha['bottom-left-square'], linha['bottom-middle-square'], linha['bottom-right-square']]
+    ]
+    
+    # Verificar se "O" ganhou
+    if (any([linha == (0, 0, 0) for linha in tabuleiro]) or  # Linhas
+        any([coluna == (0, 0, 0) for coluna in zip(*tabuleiro)]) or  # Colunas
+        [tabuleiro[i][i] for i in range(3)] == (0, 0, 0) or  # Diagonal principal
+        [tabuleiro[i][2-i] for i in range(3)] == (0, 0, 0)):  # Diagonal secundária
+        return 0  # X perdeu (O ganhou)
+    
+
+    # Verificar se "X" ganhou
+    if (any([linha == (1, 1, 1) for linha in tabuleiro]) or  # Linhas
+        any([coluna == (1, 1, 1) for coluna in zip(*tabuleiro)]) or  # Colunas
+        [tabuleiro[i][i] for i in range(3)] == (1, 1, 1) or  # Diagonal principal
+        [tabuleiro[i][2-i] for i in range(3)] == (1, 1, 1)):  # Diagonal secundária
+        return 1  # X ganhou
+    
+    # Verificar se há espaços vazios
+    if any(2 in linha for linha in tabuleiro):
+        return 3  # Ainda tem jogo
+    
+    # Se nenhuma condição anterior for atendida, é um empate
+    return 2  # Empate
+
+# Ler o arquivo CSV
+df = pd.read_csv('tic-tac-toe-no-classes.data', header=None)
+
+df.columns = ['top-left-square', 'top-middle-square', 'top-right-square',
+              'middle-left-square', 'middle-middle-square', 'middle-right-square',
+              'bottom-left-square', 'bottom-middle-square', 'bottom-right-square', 'result']
+
+# Atualizar a última coluna com base no resultado do jogo
+df['result'] = df.apply(verificar_resultado, axis=1)
+
+# Salvar o DataFrame atualizado em um novo arquivo
+df.to_csv('tic-tac-toe-processed.data', index=False)
+
+print("Última coluna atualizada com os resultados.")
--- knntictactoe.py
+++ knntictactoe.py
@@ -1,15 +1,18 @@
 import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 def inicia_algoritmo():
-    data = pd.read_csv('./tic-tac-toe-processed.data', sep=',', header=0)
+    X = pd.read_csv('./tic-tac-toe-processed.data', sep=',', header=0)
+    y = pd.read_csv('./tic-tac-toe-processed.data', sep=',', header=0)
+    
+    X = X.drop(X.columns[9], axis=1)
+    y = y.drop(y.columns[:9], axis=1)
 
-    X = data
-    y = data['result']
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.50)
 
     scaler = StandardScaler()
     scaler.fit(X_train)
@@ -17,14 +20,15 @@ def inicia_algoritmo():
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
 
-    knn = KNeighborsClassifier(n_neighbors=6)
-    knn.fit(X_train, y_train)
+    knn = KNeighborsClassifier(n_neighbors=8)
+    knn.fit(X_train, np.ravel(y_train))
 
-    return knn
+    y_pred = knn.predict(X_test)
 
-def retorna_previsao_knn(tabuleiro):
-    knn = inicia_algoritmo()
+    # print(y_pred)
+    # print([1,1,1,1,0,0,1,0,0])
 
-    previsao = knn.predict(tabuleiro)
-    
-    return previsao
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+
+inicia_algoritmo()
